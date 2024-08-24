be2ce207df3c5305735c5654aa00da028115434c
Martin A. Ortega <lynxestudio@outlook.com>
2024-08-24 01:04:18 -0600

    Adds source code to the project

---
+++ src/Manager.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Samples.OOP
+{
+	public class Manager : Employee
+	{
+  		private string m_Departament;
+ 
+  		public string Departament
+  		{
+    		get { return m_Departament; }
+    		set { m_Departament = value; }
+  		}
+
+		public override string GetDetails()
+		{
+			return "Name: " + Name + Environment.NewLine +
+			"Salary: " + Salary + Environment.NewLine +
+			"Manager of: " + Departament;
+		}
+	}
+}
---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Samples.OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Martin Marquez";
+            employee.Salary = 300.49d;
+            employee.BirthDate = DateTime.Parse("2011-11-04");
+
+            Manager manager = new Manager();
+            manager.Name = "Katharina Marquez";
+            manager.Salary = 14000.89;
+            manager.Departament = "Sales";
+            
+
+            Console.WriteLine("[ Employee details ]");
+            Console.WriteLine("========================");
+            Console.WriteLine(employee.GetDetails());
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("[ Manager details ]");
+            Console.WriteLine("========================");
+            Console.WriteLine(manager.GetDetails());
+
+
+        }
+    }
+}
