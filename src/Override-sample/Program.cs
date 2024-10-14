422286a01e149f370a250e24638db90f627dcbd8
Martin A. Ortega <lynxestudio@outlook.com>
2024-10-14 12:48:46 -0600

    Adds source code to the project

---
+++ src/Override-sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Samples.OOP;
+
+Employee employee = new Employee();
+employee.Name = "Martin Marquez";
+employee.Salary = 300.49d;
+employee.BirthDate = DateTime.Parse("2011-11-04");
+
+Manager manager = new Manager();
+manager.Name = "Katharina Marquez";
+manager.Salary = 14000.89;
+manager.Departament = "Sales";
+            
+
+Console.WriteLine("[ Employee details ]");
+Console.WriteLine("========================");
+Console.WriteLine(employee.GetDetails());
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine("[ Manager details ]");
+Console.WriteLine("========================");
+Console.WriteLine(manager.GetDetails());
